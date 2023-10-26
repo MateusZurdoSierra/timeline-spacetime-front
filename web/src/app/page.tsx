34746c463296319b77bf7716335263273344818c
MateusZurdoSierra <mateus.zurdo@superlogica.com>
2023-10-26 00:08:36 -0300

    finalize left home web

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,4 +1,6 @@
 import { User } from 'lucide-react'
+import nlwLogo from '../assets/nlw-spacetime-logo.svg'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -6,10 +8,10 @@ export default function Home() {
       {/* Left */}
       <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
         {/* Blur */}
-        <div className="blur-full translate-x1/2 absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 rounded-full bg-purple-700 opacity-50" />
+        <div className="translate-x1/2 absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
 
         {/* Stripes */}
-        <div className="bg-stripes absolute bottom-0 right-2 top-0 w-2 " />
+        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes " />
 
         {/* Sign In */}
         <a
@@ -25,6 +27,41 @@ export default function Home() {
             memórias!
           </p>
         </a>
+
+        {/* Hero */}
+        <div className="space-y-5">
+          <Image src={nlwLogo} alt="NLW LOGO" />
+
+          <div className="max-w-[420px] space-y-1">
+            <h1 className="text-5xl font-bold leading-tight text-gray-50">
+              Sua cápsula do tempo
+            </h1>
+            <p className="text-lg leading-relaxed">
+              Colecione momentos marcantes da sua jornada e compartilhe (se
+              quiser) com o mundo!
+            </p>
+          </div>
+
+          <a
+            href=""
+            className="font-all inline-block rounded-full bg-green-500 px-5 py-3 text-sm uppercase leading-none text-black hover:bg-green-600"
+          >
+            CADASTRAR LEMBRANÇA
+          </a>
+        </div>
+
+        {/* Copyright */}
+        <div className="text-sm leading-relaxed text-gray-200">
+          Feito com 💜 no NLW da{' '}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className="underline hover:text-gray-100"
+            href="https://rocketseat.com.br"
+          >
+            Rocketseat
+          </a>
+        </div>
       </div>
 
       {/* Right */}
